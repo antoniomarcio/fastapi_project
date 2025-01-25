9fa7447b67ab90b2d4f54df3d74ab74ab883c551
Temp Account <woriwi2035@laserlip.com>
2025-01-24 21:52:22 -0300

    Testando o post

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,15 +1,37 @@
 from http import HTTPStatus
 
+import pytest
 from fastapi.testclient import TestClient
 
 from fast_zero.app import app
 
 
-def test_read_root_deve_retornar_ok_e_ola_mundo():
-    client = TestClient(app)  # Arrange (organização)
+@pytest.fixture
+def client():
+    return TestClient(app=app)
 
-    response = client.get('/')  # Act (ação)
 
-    assert response.status_code == HTTPStatus.OK  # Assert (afirmação)
+def test_read_root_deve_retornar_ok_e_ola_mundo(client):  # Arrange (organizar)
+    response = client.get('/')  # Act (agir)
 
-    assert response.json() == {'message': 'Olá mundo!'}  # Assert (afirmação)
+    assert response.status_code == HTTPStatus.OK  # Assert (afirmar)
+
+    assert response.json() == {'message': 'Olá mundo!'}  # Assert (afirmar)
+
+
+def test_create_user(client):
+    response = client.post(
+        '/users/',
+        json={
+            'username': 'test_username',
+            'password': 'test_password',
+            'email': 'test@email.com',
+        },
+    )
+
+    assert response.status_code == HTTPStatus.CREATED
+    assert response.json() == {
+        'username': 'test_username',
+        'email': 'test@email.com',
+        'id': 1,
+    }
