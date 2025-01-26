3da0c639fc5e0760c87c8c5303cafdde7101b9da
antoniomarcio <a.marcio00@gmail.com>
2025-01-25 23:40:59 -0300

    Criando o método POST e adicionando testes

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,15 +1,44 @@
 from http import HTTPStatus
 
-from fastapi.testclient import TestClient
 
-from fast_zero.app import app
-
-
-def test_read_root_deve_retornar_ok_e_ola_mundo():
-    client = TestClient(app)  # Arrange (organização)
-
+def test_read_root__retornar_ok_e_ola_mundo(client):  # Arrange (organização)
     response = client.get('/')  # Act (ação)
 
     assert response.status_code == HTTPStatus.OK  # Assert (afirmação)
 
     assert response.json() == {'message': 'Olá mundo!'}  # Assert (afirmação)
+
+
+def test_create_user(client):
+    response = client.post(
+        '/users/',
+        json={
+            'username': 'test',
+            'password': 'testtest',
+            'email': 'test@test.com',
+        },
+    )
+
+    assert response.status_code == HTTPStatus.CREATED
+
+    assert response.json() == {
+        'username': 'test',
+        'email': 'test@test.com',
+        'id': 1,
+    }
+
+
+def test_read_users(client):
+    response = client.get('/users/')
+
+    assert response.status_code == HTTPStatus.OK
+
+    assert response.json() == {
+        'users': [
+            {
+                'username': 'test',
+                'email': 'test@test.com',
+                'id': 1,
+            }
+        ]
+    }
